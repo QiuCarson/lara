0c0fdd015bcb7ed04a57a176ae553227501b347b
qiuyisong <qiuyisong@gmail.com>
2016-10-10 17:34:06 +0800

    first commit

---
+++ app/Models/Student.php
@@ -0,0 +1,6 @@
+<?php
+namespace APP\Models;
+use Illuminate\Database\Eloquent\Model;
+class Student extends Model{
+    public $timestamps=false;
+}
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' =>'mysql',
+    'host'=>'127.0.0.1',
+    'database'=>'lara',
+    'username'=>'lara',
+    'password'=>'123456',
+    'charset'=>'utf8',
+    'collation'=>'utf8_general_ci',
+    'prefix'=>''
+];
